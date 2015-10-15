b49565606b099afe4b29fae1a3caba2e7c55d338
Emnet Gossaye <emnetg@uw.edu>
2015-10-15 15:16:28 -0700

    added Job.swift class

---
+++ DomainModeling/Job.swift
@@ -0,0 +1,37 @@
+//
+//  Job.swift
+//  DomainModeling
+//
+//  Created by user on 10/15/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+import Foundation
+
+class Job {
+    
+    var title : String
+    
+    var salary : Double
+    
+    var hourly : Bool
+    
+    init(jobTitle : String, jobSalary : Double, hourly: Bool) {
+        self.title = jobTitle
+        self.salary = jobSalary
+        self.hourly = hourly
+    }
+    
+    func calculateIncome(hours h : Double) -> Double {
+        if (hourly) {
+            return h * salary
+        } else {
+            return salary
+        }
+    }
+    
+    func raise(rate r: Int) -> Void {
+        let percentage = Double(r / 100)
+        self.salary += (self.salary * percentage)
+    }
+}
