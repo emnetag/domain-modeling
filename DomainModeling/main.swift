e565c947430cad41d2e022cc841cd4524348bcba
Emnet Gossaye <emnetg@uw.edu>
2015-10-15 14:26:04 -0700

    money money money

--- DomainModeling/main.swift
+++ DomainModeling/main.swift
@@ -11,25 +11,89 @@ import Foundation
 print("Hello, World!")
 
 struct Money {
-    var amount : Int = 0
+    var amount : Double = 0
     
-    enum CurrencyType {
+    enum Currency {
         case GBP
         case USD
         case CAN
         case EUR
     }
     
-    mutating func add(amount a: Int) -> Void {
+    mutating func add(amount a: Double, currency c: Currency) -> Void {
+        if (self.currency != c) {
+            convert(type: c)
+        }
         self.amount += a
     }
     
-    mutating func subtract(amount a: Int) -> Void {
+    mutating func subtract(amount a: Double, currency c: Currency ) -> Void {
+        if (self.currency != c) {
+            convert(type: c)
+        }
         self.amount -= a
     }
     
-    var currency = CurrencyType.USD
+    var currency = Currency.USD
     
-    
+    mutating func convert(type t: Currency) -> Void {
+        switch self.currency {
+            case .USD:
+                switch t {
+                    case .GBP:
+                        self.amount *= 0.5
+                    case .CAN:
+                        self.amount *= 1.25
+                    case .EUR:
+                        self.amount *= 1.5
+                    case .USD:
+                        return
+                }
+            case .GBP:
+                switch t {
+                    case .GBP:
+                        return
+                    case .CAN:
+                        self.amount *= 2.5
+                    case .EUR:
+                        self.amount *= 3
+                    case .USD:
+                        self.amount *= 2
+                }
+            case .EUR:
+                switch t {
+                    case .GBP:
+                        self.amount *= 0.33
+                    case .CAN:
+                        self.amount *= 0.83
+                    case .USD:
+                        self.amount *= 0.667
+                    case .EUR:
+                        return
+                }
+            case .CAN:
+                switch t {
+                    case .GBP:
+                        self.amount *= 0.4
+                    case .USD:
+                        self.amount *= 0.8
+                    case .EUR:
+                        self.amount *= 1.2
+                    case .CAN:
+                        return
+                }
+        }
+        self.currency = t
+    }
 }
+
+var wallet = Money()
+wallet.add(amount: 20, currency: .USD)
+
+wallet.convert(type: .USD)
+
+print("\(wallet.currency)")
+
+print("I got \(wallet.amount) \(wallet.currency) in my wallet")
+
 
